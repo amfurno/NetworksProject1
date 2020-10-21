bbf8a12aea997e17252ca75b481a77c91bf0fd98
Basil Wells <bhw0010@auburn.edu>
2020-10-21 14:25:19 -0500

    Added unp.h

--- Server/server.cpp
+++ Server/server.cpp
@@ -2,3 +2,8 @@
 // Created by amfur on 10/21/2020.
 //
 
+#include <iostream>
+#include <sys/types.h>
+#include "unp.h"
+
+
---
+++ unp.h
@@ -0,0 +1,12 @@
+//
+// Created by bhold on 10/21/2020.
+//
+
+#ifndef NETWORKSPROJECT1_UNP_H
+#define NETWORKSPROJECT1_UNP_H
+
+int const SERVPORT = 21
+int const MAXLINE = 4096
+int const BUFFSIZE = 8192
+
+#endif //NETWORKSPROJECT1_UNP_H
