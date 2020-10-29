22e9813ab98d7d33993245f808fc482f088da541
Eli Canup <elicanup@aol.com>
2020-10-29 01:24:28 -0500

    draft gremlin
    drafted the gremlin function doesn't damage the packet yet, but does return a bool for if we should drop the packet

--- Client/gremlin.cpp
+++ Client/gremlin.cpp
@@ -3,3 +3,23 @@
 //
 
 #include "gremlin.h"
+#include <random>
+#include <functional>
+
+
+bool gremlin(uint8_t *packet, float damage, float drop) {
+	std::random_device g;
+	std::uniform_real_distribution<float> dist(0,1);
+
+	float result = dist(g);
+	if (result > drop) {
+		return false;
+	}
+
+	result = dist(g);
+	if (result > damage) { //damage packet
+		//TODO: Damage packet
+	}
+	return true;
+}
+
--- Client/gremlin.h
+++ Client/gremlin.h
@@ -5,9 +5,10 @@
 #ifndef NETWORKSPROJECT1_GREMLIN_H
 #define NETWORKSPROJECT1_GREMLIN_H
 #include "unp.h"
-
+#include <cstdint>
 
 //modifies the packet
-void gremlin(packet *p, float damage, float drop);
+bool gremlin(uint8_t *p, float damage, float drop);
+
 
 #endif //NETWORKSPROJECT1_GREMLIN_H
