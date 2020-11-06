f48423f732099239d0c2f3ada35d5bffabc10813
Anthony <hockeyamf@gmail.com>
2020-11-05 19:45:04 -0600

    makePacket

--- Client/client.cpp
+++ Client/client.cpp
@@ -7,11 +7,12 @@
 #include <vector>
 #include "../unp.h"
 #include "gremlin.h"
+#include <cstdint>
 
 using namespace std;
 
 void sendPacket(); //TODO write this
-void makePacket(); //TODO write this
+void makePacket(uint8_t packet[], uint16_t packetNumber, vector<uint8_t> data);
 
 int main (int argc, char *argv[]) {
 //arguments expected [filename, errorChance]
@@ -20,30 +21,54 @@ int main (int argc, char *argv[]) {
 
     if (!inputFile.is_open()) {
         cout << "Unable to open file, exiting";
-        return 1;
+        exit(1);
     }
     else {
         cout << "file opened successfully.\n";
     }
 
-    char ch;
-    vector<char> packetData;
+    uint8_t ch;
+    vector<uint8_t> packetData;
+    uint8_t packet[128] = {0};
+    uint16_t packetNumber = 0;
+
     while (inputFile >> noskipws >> ch) {
         packetData.push_back(ch);
-        if (packetData.size() == 110) {
-            makePacket();
-            gremlin();//TODO add proper function call
-            sendPacket();
+        if (packetData.size() == 124) {
+            makePacket(packet, packetNumber, packetData);
+            if (gremlin()) {//TODO add proper function call
+                sendPacket();
+            }
+            packetNumber++;
             packetData.clear();
         }
     }
-    packetData.resize(110, '\0');
-    makePacket();
-    gremlin();//TODO add proper function call
-    sendPacket();
-
+    if (!packetData.empty()) {
+        packetData.resize(124, NULL);
+        makePacket();
+        if (gremlin()) {//TODO add proper function call
+            sendPacket();
+        }
+    }
     //TODO send 1 Byte packet
 
 
+
     return 0;
+}
+
+void makePacket(uint8_t packet[], uint16_t packetNumber, vector<uint8_t> data) {
+    int packetLocation = 4;
+    for (int i = 0; i < data.size(); i++) {
+        packet[packetLocation] = data[i];
+    }
+
+    uint8_t packNum1 = (packetNumber >> (8*0)) & 0xff;
+    uint8_t packNum2 = (packetNumber >> (8*1)) & 0xff;
+    packet[0] = packNum1;
+    packet[1] = packNum2;
+
+    uint16_t checksumValue = checksum(packet);
+    uint8_t checkSum1 = (checksumValue >> (8*0)) & 0xff;
+    uint8_t checkSum2 = (checksumValue >> (8*1)) & 0xff;
 }
